192313cd1d81b47054d75b867b4e60049b541c6b
Nils Sommer <mail@nilssommer.de>
2015-04-23 20:35:55 +0200

    Added logger to bootstrap class.

--- src/verteilteSysteme/Bootstrap.java
+++ src/verteilteSysteme/Bootstrap.java
@@ -1,15 +1,19 @@
 package verteilteSysteme;
 
-public class Bootstrap {
-	public static void main(String[] args) {
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+public class Bootstrap {
+	private static final Logger logger = LoggerFactory.getLogger(Bootstrap.class);
+	
+	public static void main(String[] args) {
 		try {
-			ChatWindow window = new ChatWindow("klaus1");
-			Thread pollthread;
-			pollthread = new Thread(new PollThread(window));
+			final ChatWindow window = new ChatWindow("klaus1");
+			logger.info("Window created.");
+			final Thread pollthread = new Thread(new PollThread(window));
 			pollthread.start();
+			logger.info("Started listening for new messages.");
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 		
