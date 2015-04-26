04fbfab9359a57274e32a16d0c5054a025e03350
Nils Sommer <mail@nilssommer.de>
2015-04-26 15:19:39 +0200

    Add singleton to get a db connector instance.

---
+++ src/verteilteSysteme/couchdb/CouchDbConnection.java
@@ -0,0 +1,42 @@
+package verteilteSysteme.couchdb;
+
+import java.net.MalformedURLException;
+
+import org.ektorp.CouchDbConnector;
+import org.ektorp.CouchDbInstance;
+import org.ektorp.http.HttpClient;
+import org.ektorp.http.StdHttpClient;
+import org.ektorp.impl.StdCouchDbConnector;
+import org.ektorp.impl.StdCouchDbInstance;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public final class CouchDbConnection {
+	private static CouchDbConnector db = null;
+	
+	private static final Logger logger = LoggerFactory.getLogger(CouchDbConnection.class);
+	
+	private CouchDbConnection() {}
+	
+	public static synchronized CouchDbConnector get() {
+		if (db == null) {
+			final HttpClient client = buildHttpClientQuietly();
+			final CouchDbInstance dbInstance = new StdCouchDbInstance(client);
+			final CouchDbConnector db = new StdCouchDbConnector("test-db", dbInstance);
+			db.createDatabaseIfNotExists();
+		}
+		
+		return db;
+	}
+	
+	private static HttpClient buildHttpClientQuietly() {
+		try {
+			return new StdHttpClient.Builder()
+						.url("http://127.0.0.1:5984/")
+						.build();
+		} catch (MalformedURLException e) {
+			logger.error("Unexpected MalformedURLException.");
+			return null;
+		}
+	}
+}
