8acd16c2f36f60e5856b0823681c9122cf59b945
Nils Sommer <mail@nilssommer.de>
2015-04-29 13:02:22 +0200

    Add ability to check if a username is reserved.

--- src/verteilteSysteme/couchdb/User.java
+++ src/verteilteSysteme/couchdb/User.java
@@ -24,4 +24,14 @@ public class User extends CouchDbDocument {
 	public void setName(String name) {
 		this.name = name;
 	}
+	
+	@Override
+	public String toString() {
+		return this.name;
+	}
+	
+	@Override
+	public int hashCode() {
+		return this.name.hashCode();
+	}
 }
--- src/verteilteSysteme/couchdb/UserRepository.java
+++ src/verteilteSysteme/couchdb/UserRepository.java
@@ -1,27 +1,24 @@
 package verteilteSysteme.couchdb;
 
-import java.util.List;
-
 import org.ektorp.CouchDbConnector;
 import org.ektorp.support.CouchDbRepositorySupport;
-import org.ektorp.support.GenerateView;
 
 public class UserRepository extends CouchDbRepositorySupport<User>{
 	public UserRepository(final CouchDbConnector connector) {
 		super(User.class, connector);
 	}
 	
-//	@GenerateView
-//	public List<User> findByTag(String tag) {
-//		return queryView("by_tag", tag);
-//	}
-//	
-//	@Override
-//	public List<User> getAll() {
-//		return this.findByTag("user_document");
-//	}
-	
 	public int getNrOfUsers() {
 		return this.getAll().size();
+	}
+	
+	public boolean exists(final User userToCheck) {
+		for (User user : this.getAll()) {
+			if (user.equals(userToCheck)) {
+				return true;
+			}
+		}
+		
+		return false;
 	}
 }
